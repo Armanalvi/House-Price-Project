745fa687cbef0e4e4669cbe7361f1189d62267b6
Arman-Alvi <96648260+Armanalvi@users.noreply.github.com>
2026-04-25 09:50:19 +0530

    Update house.py

--- house.py
+++ house.py
@@ -1,1 +1,9 @@
+def predict_price(area, bedrooms):
+    price = (area * 3000) + (bedrooms * 500000)
+    return price
 
+
+if __name__ == "__main__":
+    area = int(input("Enter area: "))
+    bedrooms = int(input("Enter bedrooms: "))
+    print("Estimated Price:", predict_price(area, bedrooms))
